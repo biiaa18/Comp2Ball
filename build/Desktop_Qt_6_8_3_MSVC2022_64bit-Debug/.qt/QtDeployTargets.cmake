743a59f88e1cd6e7c51265cf4852dc644ca7729f
Candidate_514_23DPRO101 <splunch18@gmail.com>
2025-10-27 14:45:29 +0100

    delete build

--- build/Desktop_Qt_6_8_3_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_QtVulkanApp_FILE C:/UnrealProjects/PhysicsRollingBall/Comp2Ball/build/Desktop_Qt_6_8_3_MSVC2022_64bit-Debug/QtVulkanApp.exe)
-set(__QT_DEPLOY_TARGET_QtVulkanApp_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_QtVulkanApp_RUNTIME_DLLS C:/Qt/6.8.3/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.8.3/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.8.3/msvc2022_64/bin/Qt6Cored.dll)
